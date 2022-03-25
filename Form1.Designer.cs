3826a0f3e19fbb00460bbbf4becc3485f2397062
BuildTools <unconfigured@null.spigotmc.org>
2022-03-26 01:39:58 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace appsizerGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.window = new System.Windows.Forms.ComboBox();
+            this.b = new System.Windows.Forms.Label();
+            this.r = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.h = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.w = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.y = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.x = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.h)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.w)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.y)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Menu;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(329, 24);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem5});
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(37, 20);
+            this.toolStripMenuItem4.Text = "File";
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem5.Text = "Exit";
+            this.toolStripMenuItem5.Click += new System.EventHandler(this.exit);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripSeparator1,
+            this.toolStripMenuItem6,
+            this.toolStripMenuItem3});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(54, 20);
+            this.toolStripMenuItem1.Text = "Action";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(154, 22);
+            this.toolStripMenuItem2.Text = "Refresh";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.refreshPos);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(151, 6);
+            // 
+            // toolStripMenuItem6
+            // 
+            this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(154, 22);
+            this.toolStripMenuItem6.Text = "Set";
+            this.toolStripMenuItem6.Click += new System.EventHandler(this.setPos);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(154, 22);
+            this.toolStripMenuItem3.Text = "Center window";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.centerWin);
+            // 
+            // window
+            // 
+            this.window.FormattingEnabled = true;
+            this.window.Location = new System.Drawing.Point(12, 30);
+            this.window.Name = "window";
+            this.window.Size = new System.Drawing.Size(305, 21);
+            this.window.TabIndex = 14;
+            this.window.Text = "Type or select a window by title...";
+            this.window.DropDown += new System.EventHandler(this.listWin);
+            this.window.SelectedIndexChanged += new System.EventHandler(this.refreshPos);
+            // 
+            // b
+            // 
+            this.b.AutoSize = true;
+            this.b.Location = new System.Drawing.Point(282, 90);
+            this.b.Name = "b";
+            this.b.Size = new System.Drawing.Size(13, 13);
+            this.b.TabIndex = 25;
+            this.b.Text = "0";
+            // 
+            // r
+            // 
+            this.r.AutoSize = true;
+            this.r.Location = new System.Drawing.Point(282, 64);
+            this.r.Name = "r";
+            this.r.Size = new System.Drawing.Size(13, 13);
+            this.r.TabIndex = 26;
+            this.r.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(233, 90);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Bottom:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(241, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Right:";
+            // 
+            // h
+            // 
+            this.h.Location = new System.Drawing.Point(163, 88);
+            this.h.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.h.Minimum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            -2147483648});
+            this.h.Name = "h";
+            this.h.Size = new System.Drawing.Size(60, 20);
+            this.h.TabIndex = 22;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(116, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Height:";
+            // 
+            // w
+            // 
+            this.w.Location = new System.Drawing.Point(163, 62);
+            this.w.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.w.Minimum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            -2147483648});
+            this.w.Name = "w";
+            this.w.Size = new System.Drawing.Size(60, 20);
+            this.w.TabIndex = 20;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(119, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Width:";
+            // 
+            // y
+            // 
+            this.y.Location = new System.Drawing.Point(35, 88);
+            this.y.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.y.Minimum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            -2147483648});
+            this.y.Name = "y";
+            this.y.Size = new System.Drawing.Size(60, 20);
+            this.y.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Y:";
+            // 
+            // x
+            // 
+            this.x.Location = new System.Drawing.Point(35, 62);
+            this.x.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.x.Minimum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            -2147483648});
+            this.x.Name = "x";
+            this.x.Size = new System.Drawing.Size(60, 20);
+            this.x.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "X:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 121);
+            this.Controls.Add(this.b);
+            this.Controls.Add(this.r);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.h);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.w);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.y);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.x);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.window);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "appsizerGUI";
+            this.TopMost = true;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.h)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.w)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.y)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
+        private System.Windows.Forms.ComboBox window;
+        private System.Windows.Forms.Label b;
+        private System.Windows.Forms.Label r;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown h;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown w;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown y;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown x;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace appsizerGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static IntPtr getWinHandle(string wName)
+        {
+            foreach (Process pList in Process.GetProcesses())
+                if (pList.MainWindowTitle == wName)
+                    return pList.MainWindowHandle;
+
+            return IntPtr.Zero;
+        }
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hwnd, ref Rect rectangle);
+        public struct Rect
+        {
+            public int Left { get; set; }
+            public int Top { get; set; }
+            public int Right { get; set; }
+            public int Bottom { get; set; }
+        }
+        IntPtr handle;
+        public void updateRightBottom()
+        {
+            int screenwidth = Screen.PrimaryScreen.Bounds.Width;
+            int screenheight = Screen.PrimaryScreen.Bounds.Height;
+            r.Text = (screenwidth - x.Value - w.Value).ToString();
+            b.Text = (screenheight - y.Value - h.Value).ToString();
+        }
+        public void removeValueChangedHandler()
+        {
+            x.ValueChanged -= new EventHandler(setPos);
+            y.ValueChanged -= new EventHandler(setPos);
+            w.ValueChanged -= new EventHandler(setPos);
+            h.ValueChanged -= new EventHandler(setPos);
+        }
+        public void addValueChangedHandler()
+        {
+            x.ValueChanged += new EventHandler(setPos);
+            y.ValueChanged += new EventHandler(setPos);
+            w.ValueChanged += new EventHandler(setPos);
+            h.ValueChanged += new EventHandler(setPos);
+        }
+        private void refreshPos(object sender, EventArgs e)
+        {
+            removeValueChangedHandler();
+            string windowname = window.Text;
+            handle = getWinHandle(windowname);
+            Rect pos = new Rect();
+            GetWindowRect(handle, ref pos);
+            x.Value = pos.Left + 7;
+            y.Value = pos.Top;
+            w.Value = pos.Right - pos.Left - 14;
+            h.Value = pos.Bottom - pos.Top;
+            updateRightBottom();
+            addValueChangedHandler();
+        }
+        [DllImport("user32.dll")]
+        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+        private void setPos(object sender, EventArgs e)
+        {
+            int left = (int)x.Value;
+            int top = (int)y.Value;
+            int width = (int)w.Value;
+            int height = (int)h.Value;
+            updateRightBottom();
+            SetWindowPos(handle, IntPtr.Zero, left - 7, top, width + 14, height, 0);
+        }
+        private void centerWin(object sender, EventArgs e)
+        {
+            removeValueChangedHandler();
+            int screenwidth = Screen.PrimaryScreen.Bounds.Width;
+            int screenheight = Screen.PrimaryScreen.Bounds.Height;
+            int width = (int)w.Value;
+            int height = (int)h.Value;
+            int left = (screenwidth - width) / 2;
+            int top = (screenheight - height) / 2;
+            x.Value = left;
+            y.Value = top;
+            updateRightBottom();
+            SetWindowPos(handle, IntPtr.Zero, left - 7, top, width + 14, height, 0);
+            addValueChangedHandler();
+        }
+
+        private void listWin(object sender, EventArgs e)
+        {
+            window.Items.Clear();
+            foreach (Process pList in Process.GetProcesses())
+                if (!String.IsNullOrEmpty(pList.MainWindowTitle))
+                    window.Items.Add(pList.MainWindowTitle);
+        }
+
+        private void exit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
